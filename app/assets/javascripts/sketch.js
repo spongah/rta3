98ac45ed910992e84d3860fe0a5f12060195950b
spongah <rocksolid801@gmail.com>
2016-08-06 23:40:42 -0600

    working options for 26 band or 45 band

--- app/assets/javascripts/sketch.js
+++ app/assets/javascripts/sketch.js
@@ -1,3 +1,28 @@
+
+// PULLS PARAMETERS
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+  return query_string;
+}();
+
 var mic;
 var fft;
 var maxLevel= 0;
@@ -6,12 +31,23 @@ var oneThirdFreqArrayOptimized = [50,63,80,100,125,160,200,250,315,400,500,630,8
 var oneSixthFreqArray = [50,56,63,72,81,92,104,118,133,150,170,192,216,245,277,313,353,400,452,510,576,651,735,831,940,1062,1200,1356,1532,1739,1955,2210,2498,2822,3189,3604,4072,4600,5200,5875,6640,7502,8478,9580,10825,12232,13822,15620,17650,20000];
 var oneSixthFreqArrayOptimized = [50,63,81,104,133,150,170,192,216,245,277,313,353,400,452,510,576,651,735,831,940,1062,1200,1356,1532,1739,1955,2210,2498,2822,3189,3604,4072,4600,5200,5875,6640,7502,8478,9580,10825,12232,13822,15620,17650,20000];
 
-var freqArray = oneSixthFreqArrayOptimized;  	// SET NUMBER OF BARS HERE
-var energyFactor = 0.2; 							// SET ENERGY MULTIPLIER HERE
+var freqArray = [];
+var barWidth = 0;
+
+if (QueryString.bands == 26) {
+	freqArray = oneThirdFreqArrayOptimized;
+	barWidth = parseInt(100 / freqArray.length) * 0.9;
+} else if (QueryString.bands == 45) {
+	freqArray = oneSixthFreqArrayOptimized;
+	barWidth = parseInt(100 / freqArray.length) * 0.78;
+} else {
+	freqArray = oneThirdFreqArrayOptimized;  		// DEFAULT
+}
+
+var energyFactor = 0.2; 											// SET ENERGY MULTIPLIER HERE
 var barMultiplier = 7;
 var barFloor = 10;
 
-var barWidth = parseInt(100 / freqArray.length) * 0.78;
 var barHeight = 0;
 
 
@@ -19,13 +55,14 @@ function setup() {
 	mic = new p5.AudioIn();
 	mic.start();
 	fft = new p5.FFT();
-	fft.setInput(mic); //moved from draw
-	for (x=0;x<freqArray.length;x++) {
+	fft.setInput(mic);
+
+	for (x=0;x<freqArray.length;x++) {  // CREATE RTA BARS
 		document.getElementById("rta").innerHTML += '<div class="rtabar" id="bar' + x + '"></div>';
 		document.getElementById('bar' + x).style.width = barWidth.toString() + "%"
-		//document.getElementById('bar' + x).innerHTML = freqArray[x];
 	}
-	for (x=0;x<freqArray.length;x++) {
+
+	for (x=0;x<freqArray.length;x++) {	// CREATE LABELS
 		labelNum = freqArray[x];
 		if (labelNum > 999) {
 			labelNum = (labelNum / 1000).toPrecision(2).toString() + "k";
@@ -46,7 +83,14 @@ function drawRTA() {
 		barHeight = parseInt(fft.getEnergy(freqArray[x]) * energyFactor);
 		barHeightPercent = ((barHeight * barMultiplier) + barFloor) * 0.25;
 		if (barHeight > 100) { barHeight = 100 };
+		barElement = document.getElementById('bar' + x);
 		//document.getElementById('bar' + x).style.marginTop = barHeight.toString() + "%";
-		document.getElementById('bar' + x).style.height = barHeightPercent.toString() + "%";
+		if ((typeof(barElement) != 'undefined') && (barElement != null)) {
+			barElement.style.height = barHeightPercent.toString() + "%";
+		}
 	}
 }
+
+
+
+
